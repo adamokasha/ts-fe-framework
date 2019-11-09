82a31fb08325a698589a98745eca73bc88540ce6
Adam Okasha <adam@bendigi.com>
2019-11-09 15:41:40 -0500

    Refactor for composition

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,7 @@ import { User } from "./models/User";
 
 const user = new User({ name: "New Record", age: 0 });
 
-// user.set({ name: "New Name", age: 33 });
-user.save();
+console.log(user.get("name"));
+
+user.on("change", () => console.log("user was changed"));
+user.set({ name: "New Name" });
